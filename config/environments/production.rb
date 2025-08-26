69972c168fd25e35842814aadeac8fb314ee6a4e
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-26 17:42:56 +0900

    feat: prepare initial deploy (pages#top, prod DB via DATABASE_URL, render host allow)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << ENV["RENDER_EXTERNAL_HOSTNAME"] if ENV["RENDER_EXTERNAL_HOSTNAME"].present?
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
