2d524c9bf5a65d7e7817744c4769f3d07bb8082e
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-27 12:02:30 +0900

    feat: add validations for User (nickname presence, password format)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+                                 message: 'must include both letters and numbers' }
 end
