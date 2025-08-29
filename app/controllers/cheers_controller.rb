773245d409a505c212602f6978a9a386a53561d2
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-29 16:03:37 +0900

    fix: adjust cheer button UI and validation handling

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -1,28 +1,26 @@
 class CheersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_log
+  before_action :set_log # ← visible_to で可視性の範囲に限定
   before_action :forbid_self_cheer!
 
   def create
-    @cheer = current_user.cheers.build(log: @log)
+    # ユニーク制約競合にも強い書き方
+    current_user.cheers.create_or_find_by!(log: @log)
 
-    if @cheer.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_back fallback_location: @log, notice: "応援しました" }
-      end
-    else
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(dom_id(@log, :cheer), partial: "cheers/button", locals: { log: @log }) }
-        format.html { redirect_back fallback_location: @log, alert: @cheer.errors.full_messages.first }
-      end
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: @log, notice: "応援しました" }
+    end
+  rescue ActiveRecord::RecordNotUnique
+    # レース時の二重発行は成功扱いに寄せて再描画
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: @log, notice: "既に応援済みです" }
     end
   end
 
   def destroy
-    @cheer = current_user.cheers.find_by!(log: @log)
-    @cheer.destroy
-
+    current_user.cheers.where(log: @log).delete_all
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back fallback_location: @log, notice: "応援を取り消しました" }
@@ -32,7 +30,8 @@ class CheersController < ApplicationController
   private
 
   def set_log
-    @log = Log.find(params[:log_id])
+    # 不正アクセスで非可視ログが指定された場合に404
+    @log = Log.visible_to(current_user).find(params[:log_id])
   end
 
   def forbid_self_cheer!
