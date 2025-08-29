63836a497c068eb91c777ec8be0e1b994c018076
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-29 15:37:25 +0900

    feat: add associations and self-cheer validation to Cheer model

--- app/models/cheer.rb
+++ app/models/cheer.rb
@@ -1,4 +1,14 @@
 class Cheer < ApplicationRecord
   belongs_to :user
-  belongs_to :log
+  belongs_to :log, counter_cache: true
+
+  validates :user_id, uniqueness: { scope: :log_id }
+  validate  :cannot_cheer_own_log
+
+  private
+
+  def cannot_cheer_own_log
+    return if log.blank? || user.blank?
+    errors.add(:base, "自分の投稿には応援できません") if log.user_id == user_id
+  end
 end
