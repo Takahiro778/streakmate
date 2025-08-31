b2cb90307772b0d36e28d86086073afa3accbb6b
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-08-31 16:02:15 +0900

    feat(user): add has_one :setting and auto-create default setting on user creation

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,6 @@
 class Setting < ApplicationRecord
   belongs_to :user
+
+  validates :time_zone, presence: true, inclusion: { in: ActiveSupport::TimeZone.all.map(&:name) }
+  validates :bedtime_time, presence: true
 end
