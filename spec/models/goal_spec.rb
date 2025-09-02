b85e49d8be819887919d6bcf8deb45d8a3a5bf95
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-09-02 18:34:49 +0900

    test(model): add specs for Goal (validations/enums/associations)

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
-
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:goal) { build(:goal) }
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:category) }
+  it { is_expected.to define_enum_for(:visibility).with_values(%i[public followers private]).backed_by_column_of_type(:integer) }
+
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:favorites).dependent(:destroy) }
 end
