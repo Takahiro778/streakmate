aa7e25e1c40db5e8a8d27c8d30eed070a7455706
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-09-02 18:36:54 +0900

    test(model): add specs for Follow/Cheer/Comment (associations/constraints)

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:log) }
+  it { is_expected.to validate_presence_of(:body) }
 end
