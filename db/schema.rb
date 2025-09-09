a521cf339566803fb889d588a521754e93c74664
Takahiro778 <takahiro.yamamoto778@gmail.com>
2025-09-09 14:40:32 +0900

    docs: add final ER diagram to README

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_07_110230) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_08_135337) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -107,6 +107,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_07_110230) do
     t.integer "favorites_count", default: 0, null: false
     t.integer "tasks_count", default: 0, null: false
     t.integer "tasks_done_count", default: 0, null: false
+    t.string "og_image_url"
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
